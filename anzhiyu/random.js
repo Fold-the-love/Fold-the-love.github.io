fad6bfff3fbb85c749e382b1d1ba27e682c1652f
Fold-the-love <he2367991090@gmail.com>
2024-11-07 22:21:46 +0800

    Site updated: 2024-11-07 22:21:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/07/free_picture_bed/","2024/11/07/front_matter/","2024/11/07/Start/","2024/11/05/hello-world/"];function toRandomPost(){
+var posts=["2024/11/07/front_matter/","2024/11/07/Start/","2024/11/07/free_picture_bed/","2024/11/05/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
